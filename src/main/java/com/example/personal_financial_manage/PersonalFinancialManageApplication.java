5e6f1eb42eead90ec7c0a06165869e55788eee6c
muqiEric <2382539324@qq.com>
2024-12-27 01:16:31 +0800

    Add missing line termination in FamilyMemberRepository
    A newline character was added at the end of the FamilyMemberRepository file to ensure proper file formatting.

--- src/main/java/com/example/personal_financial_manage/PersonalFinancialManageApplication.java
+++ src/main/java/com/example/personal_financial_manage/PersonalFinancialManageApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = "com.example.financeReportSystem")
-@EntityScan("com.example.financeReportSystem.model")
+@SpringBootApplication
+@EntityScan
 public class PersonalFinancialManageApplication {
 
     public static void main(String[] args) {
